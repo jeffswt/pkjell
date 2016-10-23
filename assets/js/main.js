c7fc644a39f3bb0c36d8f1deb980001b1cdb006f
Geoffrey, Tang <jeffswt@outlook.com>
2016-10-24 01:57:51 +0800

    Supported (brief) posts downloading from server.

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,21 +29,65 @@ function blogClass() {
         return this;
     };
     this.json = new jsonClass(this);
+    function contentsClass(master) {
+        this.loading = false;
+        this.current_page = 0;
         this.load = function() {
             // Setting thread lock status
             if (this.loading)
                 return false;
+            if (master.json.loaded && this.current_page >= master.json.data.entries.length) {
+                var load_btn = $('#blog-contents-btn-load-more');
+                load_btn.removeClass('btn-info').addClass('btn-success');
+                load_btn.attr('disabled', 'disabled');
+                load_btn.html('No more articles');
+                return false;
+            }
             this.loading = true;
             // Changing icon status
             var load_btn = $('#blog-contents-btn-load-more');
             load_btn.removeClass('btn-info').addClass('btn-warning');
             load_btn.html('Loading...');
-            // Load data...
-            this.loading = false;
+            // Loading JSON
+            master.json.load(function(event) {
+            self = event[0];
+            master = event[1];
+            // Defining, getting sub-article
+            function load_articles(rem_count, self, master) {
+                // Recursion interrupt
+                if (rem_count <= 0)
+                    return ;
+                // Retrieving object
+                jdata = master.json.data;
+                if (self.current_page >= jdata.entries.length)
+                    return ;
+                obj = jdata.entries[self.current_page];
+                console.log(jdata.entries, self, self.current_page, obj);
+                self.current_page++;
+                // Getting data from server
+                html_event = $.get('/data/' + obj['date-id'] + '-' + obj['id'] + '-brief.html');
+                wait_html = function(self, master, html_event, load_articles) {
+                    if (html_event.readyState <= 1) {
+                        setTimeout(wait_html, 15, self, master, html_event, load_articles);
+                        return ;
+                    }
+                    // Injecting data into HTML.
+                    html_data = html_event.responseText;
+                    console.log(html_data);
+                    $('#blog-contents-end-flag').before(html_data);
+                    load_articles(rem_count - 1, self, master);
+                    return ;
+                };
+                wait_html(self, master, html_event, load_articles);
+                // Calling iteration...
+            }; load_articles(4, self, master);
+            // Setting thread lock status
+            self.loading = false;
             // Changing icon status
             var load_btn = $('#blog-contents-btn-load-more');
             load_btn.removeClass('btn-warning').addClass('btn-info');
             load_btn.html('Load more articles');
+            }, [this, master])
             // Finished procedure
             return true;
         };
