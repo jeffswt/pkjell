f547e6203aeedce21f5dfb012d8fd116e8481fbb
Geoffrey, Tang <jeffswt@outlook.com>
2016-10-24 02:43:34 +0800

    Refined main.js in a prototypal manner.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,102 +1,94 @@
 
-function blogClass() {
-    function jsonClass(master) {
-        this.loading = false;
-        this.loaded = false;
-        this.data = {};
-        this.load = function(callback, args) {
-            if (this.loaded) {
+var blog = {
+    json : {
+        loading : false,
+        loaded : false,
+        data : {},
+        load : function(callback, args) {
+            if (blog.json.loaded) {
                 callback(args);
                 return ;
             }
-            this.loading = true;
+            blog.json.loading = true;
             json_event = $.getJSON('/data/index.json');
-            wait_json = function(self, json_event, callback, args) {
+            wait_json = function(json_event, callback, args) {
                 if (json_event.readyState <= 1) {
-                    setTimeout(wait_json, 15, self, json_event, callback, args);
+                    setTimeout(wait_json, 15, json_event, callback, args);
                     return ;
                 }
-                self.loading = false;
-                self.loaded = true;
-                self.data = json_event.responseJSON;
+                blog.json.loading = false;
+                blog.json.loaded = true;
+                blog.json.data = json_event.responseJSON;
                 if (callback)
                     callback(args);
                 return ;
             };
-            wait_json(this, json_event, callback, args);
+            wait_json(json_event, callback, args);
             return ;
-        };
-        return this;
-    };
-    this.json = new jsonClass(this);
-    function contentsClass(master) {
-        this.loading = false;
-        this.current_page = 0;
-        this.load = function() {
+        }
+    },
+    contents : {
+        loading : false,
+        current_page : 0,
+        load : function() {
             // Setting thread lock status
-            if (this.loading)
+            if (blog.contents.loading)
                 return false;
-            if (master.json.loaded && this.current_page >= master.json.data.entries.length) {
+            if (blog.json.loaded && blog.contents.current_page >= blog.json.data.entries.length) {
                 var load_btn = $('#blog-contents-btn-load-more');
                 load_btn.removeClass('btn-info').addClass('btn-success');
                 load_btn.attr('disabled', 'disabled');
                 load_btn.html('No more articles');
                 return false;
             }
-            this.loading = true;
+            blog.contents.loading = true;
             // Changing icon status
             var load_btn = $('#blog-contents-btn-load-more');
             load_btn.removeClass('btn-info').addClass('btn-warning');
             load_btn.html('Loading...');
             // Loading JSON
-            master.json.load(function(event) {
-            self = event[0];
-            master = event[1];
+            blog.json.load(function(event) {
             // Defining, getting sub-article
-            function load_articles(rem_count, self, master) {
+            function load_articles(rem_count) {
                 // Recursion interrupt
                 if (rem_count <= 0)
                     return ;
                 // Retrieving object
-                jdata = master.json.data;
-                if (self.current_page >= jdata.entries.length)
+                jdata = blog.json.data;
+                if (blog.contents.current_page >= jdata.entries.length)
                     return ;
-                obj = jdata.entries[self.current_page];
-                console.log(jdata.entries, self, self.current_page, obj);
-                self.current_page++;
+                obj = jdata.entries[blog.contents.current_page];
+                blog.contents.current_page++;
                 // Getting data from server
                 html_event = $.get('/data/' + obj['date-id'] + '-' + obj['id'] + '-brief.html');
-                wait_html = function(self, master, html_event, load_articles) {
+                wait_html = function(html_event, load_articles) {
                     if (html_event.readyState <= 1) {
-                        setTimeout(wait_html, 15, self, master, html_event, load_articles);
+                        setTimeout(wait_html, 15, html_event, load_articles);
                         return ;
                     }
                     // Injecting data into HTML.
                     html_data = html_event.responseText;
-                    console.log(html_data);
                     $('#blog-contents-end-flag').before(html_data);
-                    load_articles(rem_count - 1, self, master);
+                    load_articles(rem_count - 1);
                     return ;
                 };
-                wait_html(self, master, html_event, load_articles);
+                wait_html(html_event, load_articles);
                 // Calling iteration...
-            }; load_articles(4, self, master);
+            }; load_articles(4);
             // Setting thread lock status
-            self.loading = false;
+            blog.contents.loading = false;
             // Changing icon status
             var load_btn = $('#blog-contents-btn-load-more');
             load_btn.removeClass('btn-warning').addClass('btn-info');
             load_btn.html('Load more articles');
-            }, [this, master])
+            }, [])
             // Finished procedure
             return true;
-        };
-        return this;
-    };
-    this.contents = new contentsClass(this);
-    function urlClass(master) {
-        this.args = {};
-        this.parse = function() {
+        }
+    },
+    url : {
+        args : {},
+        parse : function() {
             uri = window.location.href;
             dict = {};
             if (uri.lastIndexOf('?') > 0) {
@@ -107,12 +99,8 @@ function blogClass() {
                     dict[tuple[0]] = tuple[1];
                 }
             }
-            this.args = dict;
+            blog.url.args = dict;
             return dict;
-        };
-        return this;
-    };
-    this.url = new urlClass(this);
-    return this;
+        }
+    }
 };
-blog = new blogClass();
